5f4d1ccbe1a120b456bef98a38eb5baa723cfb65
ChrisAchinga <achinga.chris@gmail.com>
2019-11-06 19:20:03 +0300

    11/06/2019

--- ZetechProjectCenter/system/models.py
+++ ZetechProjectCenter/system/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class edAuditTrace(models.Model):
+	action_id  = models.IntegerField(unique=True)
+	act_user = models.CharField(max_length=10) 
+	act_staffid = models.CharField(max_length=10) 
+	act_descr  = models.CharField(max_length=100) 
+	act_datetime = models.DateField() 
+	act_other  = models.CharField(max_length=30) 
+
+	def __str__(self):
+		return self.act_user
